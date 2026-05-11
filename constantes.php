16cd1e92999b7211282d8008f312989a8317d2dd
Gabriel Genevro <gabrielgenevro01@gmail.com>
2026-05-11 04:38:39 -0300

    Criação da pagina de constantes e alteração do sorteio da banca para suportar as constantes

---
+++ constantes.php
@@ -0,0 +1,5 @@
+<?php
+        //constantes relacionadas a banca
+const num_av_internos = 1;   //numero de avaliadores internos na banca
+const num_av_externos = 3;   //numero de avaliadores externos na banca
+?>
